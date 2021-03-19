82842fa1c975ba45b346206571f16118c5c56a49
Julian ranieri <jumpingafterrain@gmail.com>
2021-03-19 15:38:00 -0700

    Updates

---
+++ precache-manifest.e17b5cf4b4007ff575565a073db4c7f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af611a0a841be0cc88357a3e0eb13081",
+    "url": "/julian-fed-challenge/index.html"
+  },
+  {
+    "revision": "5d7081a119794a5711c8",
+    "url": "/julian-fed-challenge/static/css/main.ea7794b2.chunk.css"
+  },
+  {
+    "revision": "c89e5bf0bfe9815b4157",
+    "url": "/julian-fed-challenge/static/js/2.8e2d73b7.chunk.js"
+  },
+  {
+    "revision": "f9c609386e85489aeee2924193025e8d",
+    "url": "/julian-fed-challenge/static/js/2.8e2d73b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d7081a119794a5711c8",
+    "url": "/julian-fed-challenge/static/js/main.fe414c98.chunk.js"
+  },
+  {
+    "revision": "bd9c3f59f2f1eaa67add",
+    "url": "/julian-fed-challenge/static/js/runtime-main.d445b51a.js"
+  }
+]);
